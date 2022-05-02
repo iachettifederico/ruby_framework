31f42826455602a5f21262ac6b1f66d69f63851e
Federico Iachetti <iachetti.federico@gmail.com>
2022-05-02 12:09:56 -0300

    Refactored spec/support/expect_example_to_have_assertions.rb

--- spec/support/expect_example_to_have_assertions.rb
+++ spec/support/expect_example_to_have_assertions.rb
@@ -1,19 +1,20 @@
 RSpec.configure do |config|
-  assertions_module = Module.new do
-    attr_accessor :assertions_have_been_run
+  config.include(
+    Module.new {
+      attr_accessor :assertions_have_been_run
 
-    def expect(*)
-      self.assertions_have_been_run = true
-      super
-    end
-  end
-
-  config.include(assertions_module)
+      def expect(*)
+        self.assertions_have_been_run = true
+        super
+      end
+    }
+  )
 
   config.after do |example|
-    expect(assertions_have_been_run).to(
-      eql(true),
-      "No assertion run in example '#{example.description}'",
-    )
+    unless assertions_have_been_run
+      no_assertions_error = RuntimeError.new("No assertion run in example '#{example.description}'")
+      no_assertions_error.set_backtrace([example.location])
+      raise no_assertions_error
+    end
   end
 end
