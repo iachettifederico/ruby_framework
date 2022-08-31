1fe714dfaffa9874b4d18233be4bde939d716f57
Federico Iachetti <iachetti.federico@gmail.com>
2022-08-31 13:31:21 -0300

    Removed hash initializer

--- config/initializers/hash.rb
+++
@@ -1,9 +0,0 @@
-class Hash
-  def deep_symbolize_keys
-    Hash.new.tap { |symbolized_hash|
-      each do |k, v|
-        symbolized_hash[k.to_sym] = v.is_a?(Hash) ? deep_symbolize_keys(v) : v
-      end
-    }
-  end
-end
