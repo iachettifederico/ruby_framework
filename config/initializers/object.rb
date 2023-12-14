0873f28bd71727d3428dd99760c8c399901ba5a0
Federico Iachetti <iachetti.federico@gmail.com>
2023-12-14 13:11:31 -0300

    Added subclass_responsibility to Object

--- config/initializers/object.rb
+++ config/initializers/object.rb
@@ -1,11 +1,11 @@
-class Object
-  def self.boolean_count(attr)
-    define_method("#{attr}_count") do
-      instance_variable_get("@#{attr}").count
-    end
+# frozen_string_literal: true
 
-    define_method("has_#{attr}?") do
-      public_send("#{attr}_count").positive?
-    end
+class Object
+  def subclass_responsibility
+    method = caller.first[/.+:in `(\w+)'/, 1]
+
+    raise SubclassResponsibility, "Please implement '#{method}' in subclass: #{self.class}\n on #{caller.first.yellow}"
   end
 end
+
+SubclassResponsibility = Class.new(RuntimeError)
