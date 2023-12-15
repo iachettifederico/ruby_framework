db37a34368d664ca326649c2bc1d8cc5349bc4b9
Federico Iachetti <iachetti.federico@gmail.com>
2023-12-15 17:49:55 -0300

    Fixed typo

--- spec/support/boolean_matchers.rb
+++ spec/support/boolean_matchers.rb
@@ -1,14 +1,15 @@
 # frozen_string_literal: true
 
-mobule RSpec
-module Core
-  class ExampleGroup
-    def expect_true(object)
-      expect(object).to eql(true)
-    end
+module RSpec
+  module Core
+    class ExampleGroup
+      def expect_true(object)
+        expect(object).to eql(true)
+      end
 
-    def expect_false(object)
-      expect(object).to eql(false)
+      def expect_false(object)
+        expect(object).to eql(false)
+      end
     end
   end
 end
