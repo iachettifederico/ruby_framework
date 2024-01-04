4b2855d7df9189218be351bcdf9b914141077255
Federico Iachetti <iachetti.federico@gmail.com>
2024-01-04 12:55:30 -0300

    Loading Dotenv on boot

--- boot.rb
+++ boot.rb
@@ -1,5 +1,12 @@
 # frozen_string_literal: true
 
+require "dotenv"
+Dotenv.load(
+  ".env.#{ENV.fetch('ENVIRONMENT_NAME')}.local",
+  ".env.#{ENV.fetch('ENVIRONMENT_NAME')}",
+  ".env",
+)
+
 require_relative "framework/paths"
 require_relative "config/settings"
 require_relative "framework/loader"
