3ccfe66a4c3480877d59d359d17f61aa291ac7a0
Federico Iachetti <iachetti.federico@gmail.com>
2024-01-23 18:09:30 -0300

    Always compiling assets

--- web/web_app.rb
+++ web/web_app.rb
@@ -5,7 +5,7 @@ require "roda"
 class WebApp < Roda
   plugin :public, root: ROOT.join("web/public").to_s
   plugin :assets, css: "index.css", js: "index.js"
-  compile_assets if ENV["ENVIRONMENT_NAME"] == "production"
+  compile_assets
 
   route do |r|
     r.root do
