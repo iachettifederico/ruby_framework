10847c49020db9167be0ec03b0ca2e8d4d821bc4
Federico Iachetti <iachetti.federico@gmail.com>
2024-01-23 20:45:42 -0300

    Adding html matchers for tests

---
+++ spec/support/html_specs.rb
@@ -0,0 +1,55 @@
+# frozen_string_literal: true
+
+require "nokogiri"
+require "rspec-html-matchers"
+
+def node(html_string)
+  Nokogiri::HTML(html_string)
+end
+
+module HtmlMatchers
+  def has_tag?(tagname, text: nil, with: {})
+    nodes = css(tagname)
+
+    node_exists = !nodes.empty?
+
+    nodes.any? { |node|
+      node_has_text?(node, text) &&
+        node_has_attributes?(node, with)
+    }
+  end
+
+  def find_tag(tagname)
+    css(tagname).first
+  end
+
+  def find_tags(tagname)
+    css(tagname)
+  end
+
+  private
+
+  def node_has_text?(node, text)
+    return true unless text
+
+    node.text == text
+  end
+
+  def node_has_attributes?(node, attributes)
+    attributes.all? { |attr, value|
+      node[attr.to_s] == value
+    }
+  end
+end
+
+[
+  Nokogiri::XML::Element,
+  Nokogiri::XML::Document,
+].each do |node_type|
+  node_type.include(HtmlMatchers)
+end
+
+RSpec.configure do |config|
+  config.before do
+  end
+end
