6f706de02385e9319cf9fe05f3f50b756d501b71
Federico Iachetti <iachetti.federico@gmail.com>
2024-01-25 19:39:13 -0300

    Refactored field

--- web/forms/fields/field.rb
+++ web/forms/fields/field.rb
@@ -8,7 +8,7 @@ module Forms
       def initialize(name, value: nil, **html_options)
         @name = name
         @value = value
-        @css_classes = Array(html_options.delete(:class))
+        @css_classes = build_css_classes(html_options.delete(:class))
         @html_options = html_options
       end
 
@@ -20,22 +20,6 @@ module Forms
           **html_options
         )
       end
-      # def initialize(name, **html_options)
-      #   @name = name
-      #   @css_class = build_class(html_options.delete(:class))
-      #   @value = html_options.delete(:value)
-      #   @html_options = default_html_options.merge(html_options)
-      # end
-
-      # def template
-      #   input(
-      #     value: value,
-      #     type:  type,
-      #     name:  name,
-      #     class: css_class,
-      #     **html_options
-      #   )
-      # end
 
       private
 
@@ -47,23 +31,13 @@ module Forms
         default_css_classes + @css_classes
       end
 
-      # def build_class(classes)
-      #   default_css_class.concat(
-      #     arrayfy_classes(classes)
-      #   )
-      # end
+      def build_css_classes(classes)
+        Array(classes)
+      end
 
       def default_css_classes
         %w[p-1 text-gray-800 placeholder:text-gray-400 border rounded]
       end
-
-      # def arrayfy_classes(classes)
-      #   Array(classes).join(" ").split(" ")
-      # end
-
-      # def default_html_options
-      #   {}
-      # end
     end
   end
 end
