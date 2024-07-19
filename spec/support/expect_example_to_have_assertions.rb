d071e998187889f7727c99892a6499e593c0377e
Federico Iachetti <iachetti.federico@gmail.com>
2024-07-19 19:24:38 -0300

    Force examples to have assertions conditiotally

--- spec/support/expect_example_to_have_assertions.rb
+++ spec/support/expect_example_to_have_assertions.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
-RSpec.configure do |config|
-  config.include(
-    Module.new do
+if ENV["EXAMPLES_MUST_HAVE_ASSERTIONS"]
+  RSpec.configure do |config|
+    config.include(
+      Module.new do
       attr_accessor :assertions_have_been_run
 
       def expect(*)
@@ -10,13 +11,14 @@ RSpec.configure do |config|
         super
       end
     end
-  )
+    )
 
-  config.after do |example|
-    unless assertions_have_been_run || example.exception
-      no_assertions_error = RuntimeError.new("No assertion run in example '#{example.description}'")
-      no_assertions_error.set_backtrace([example.location])
-      raise no_assertions_error
+    config.after do |example|
+      unless assertions_have_been_run || example.exception
+        no_assertions_error = RuntimeError.new("No assertion run in example '#{example.description}'")
+        no_assertions_error.set_backtrace([example.location])
+        raise no_assertions_error
+      end
     end
   end
 end
